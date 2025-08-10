c46b2889937a81502458e0945cb475ccdd141f7d
= <=>
2025-08-10 15:44:52 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+// let fruits = ["apple", "banana", "cherry"];
+// for(let fruit of fruits) {
+//     console.log(fruit);
+// }
+
+
+
+// Q #2
+// let colors = ["red", "green", "blue"];
+// let output = document.querySelector("#output")
+// for(let color of colors) {
+//     output.innerHTML += color + "<br>";
+// }
+
+
+
+//  Q #3
+// for(let i = 1; i <= 10; i++) {
+//     console.log(i);
+    
+// }
+
+
+// Q #4
+// for(let i = 5; i >= 1; i--) {
+//     console.log(i);
+// }
+
+
+// Q #5
+// let names = ["Ali", "Ahmed", "Hassan"];
+// let output = document.querySelector("#names");
+
+// for(let i = 0; i < names.length; i++) {
+//     output.innerHTML += names[i] + ", " + "<br>";
+// }
