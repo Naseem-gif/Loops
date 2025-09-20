297883e05cbcfeae3fe5d7fbe9b4d3e9587e04ef
Naseem-gif <nk842932@gmail.com>
2025-09-20 15:01:30 +0500

    20 Qustions completed

--- app.js
+++ app.js
@@ -34,3 +34,179 @@
 // for(let i = 0; i < names.length; i++) {
 //     output.innerHTML += names[i] + ", " + "<br>";
 // }
+
+
+
+// Q6...
+// let para = document.querySelector("#status");
+// para.innerHTML = "Loading..."
+
+
+
+
+// Q #7.
+// let sum = 0
+// let total = document.querySelector("#sum");
+// let numbers = [5, 10, 15];
+// for (let i = 0; i < numbers.length; i++ ){
+//     total.innerHTML = sum += numbers[i]
+// }
+
+
+// Q #8...
+// let cities = document.querySelector("#cities")
+// let cityArr = ["Karachi", "Lahore", "Quetta"];
+
+// for(let i = 0; i < cityArr.length; i++){
+//     cities.innerHTML = cityArr.join(" ")
+// }
+
+
+
+
+
+// Q9...
+
+// let i = 1
+// while(i <= 20){
+//     if(i % 2 === 0){
+//         console.log(i);
+        
+//     }
+//     i++
+// }
+
+
+
+
+// Q10 Create an array of numbers [45, 22, 89, 100], and use a loop to find and display the
+// largest number in a paragraph with ID max.
+
+// let numArr = [45, 22, 89, 100];
+// let maxNum = numArr[0];
+// let maxNumArr = document.querySelector("#max");
+// for(let i = 0; i < numArr.length; i++){
+//     if(numArr[i] > maxNum){
+//         maxNumArr.innerHTML = maxNum = numArr[i]
+//     }
+// }
+
+
+
+// Q 11..
+// document.querySelector("#result").innerHTML = "Task completed"
+
+
+// Q 12
+// let averageTemp = document.querySelector("#averageTemp");
+// let tempArr = [30, 35, 32, 40]
+// let average = 0
+// for(let i = 0; i < tempArr.length; i++){
+//     averageTemp.innerHTML = average += tempArr[i] / tempArr.length
+// }
+
+
+
+
+// Q 13...
+// let totalSum = document.querySelector("#totalsum");
+// sum = 0
+// for(let i = 1; i <= 100; i++){
+//     console.log(i);
+//     totalSum.innerHTML = sum += i
+// }
+
+
+
+
+// Q 14..
+// let animals = ["dog", "cat", "bird"];
+// let animalRes = document.querySelector("#animals");
+
+// for(let i = 0; i < animals.length; i++){
+//     animalRes.innerHTML = animals.join(" ")
+// }
+
+
+
+
+// Q 15
+// document.querySelector('#title').innerHTML = "Welcome to Javascript"
+
+
+
+
+
+// Q 16..
+// let i = 1
+// while(i <= 50) {
+
+//     if(i % 5 === 0){
+//         console.log(i);
+        
+//     }
+//     i++;
+// }
+
+
+
+// Q 17
+// let highScores = document.querySelector("#highscores");
+// let scoreArr = [80, 60, 90, 70]
+// for(let i = 0; i < scoreArr.length; i++ ){
+//     if(scoreArr[i] > 75){
+//         highScores.innerHTML += scoreArr[i] + " "
+//     }
+// }
+
+
+
+
+// Q 18...
+// document.querySelector("#message").innerHTML = "Updated successfully";
+
+
+
+// Q 19...
+// let subjectArr =  ["Math", "Science", "English"];
+// let subjectList = document.querySelector("#subjectlist");
+// for(let i = 0; i < subjectArr.length; i++){
+//     subjectList.innerHTML += subjectArr[i] + " "
+// }
+
+
+
+
+
+// Q 20
+// let numArr = [5, 10, 15, 20]
+// let sum = 0
+// let total = document.querySelector("#total")
+// for(let i = 10; i >= 1; i--){
+//     numArr.push(i);
+//     // console.log(numArr);
+    
+// }
+
+// for(let i = 0; i < numArr.length; i++){
+//    total.innerHTML =  sum += numArr[i]
+    
+    
+// }
+
+
+
+
+// Q 21..
+// let maxNumArr = [45, 67, 12, 89, 34];
+// let max = maxNumArr[0]
+// let maxNum = document.querySelector("#maxnum")
+// for(let i = 0; i <maxNumArr.length; i++){
+//     if(maxNumArr[i] > max){
+//         maxNum.innerHTML = max = maxNumArr[i];
+        
+//     }
+// }
+
+
+
